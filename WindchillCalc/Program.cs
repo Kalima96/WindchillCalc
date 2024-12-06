e257044df446de84707f278ab6ca515fc0fd0473
Hemen <h24hemka@du.se>
2024-12-06 13:42:32 +0100

    testar push från visual studio

--- WindchillCalc/Program.cs
+++ WindchillCalc/Program.cs
@@ -8,30 +8,10 @@
 
             while (restart)
             {
-                Console.WriteLine("Skriv in lufttemperaturen i Celsius");
-                Console.Write("\n");
-
-                string Userinput_Temp = Console.ReadLine();
-                double T = double.Parse(Userinput_Temp);
 
 
-                Console.WriteLine("Skriv in vindhastigheten i km/h");
-                Console.Write("\n");
-                string Userinput_Vind = Console.ReadLine();
-                double V = double.Parse(Userinput_Vind);
-
-                double WCT;
-
-                WCT = 13.12 + (0.6215 * T) - (11.37 * Math.Pow(V, 0.16)) + (0.3965 * T * Math.Pow(V, 0.16));
-
-                Console.Write("\n");
-                Console.WriteLine("Svara Ja för att räkna ut och Nej för att avsluta");
-                Console.Write("\n");
-                Console.WriteLine("Beräkna windchill-faktor?");
-                Console.Write("\n");
+                Console.WriteLine("Vill du starta windchillberäknaren?");
                 Console.WriteLine("Ange Ja/Nej");
-
-
 
 
 
@@ -44,7 +24,27 @@
                 switch (UserInput)
                 {
                     case "ja":
+                        Console.WriteLine("Skriv in lufttemperaturen i Celsius");
+                        Console.Write("\n");
 
+                        string Userinput_Temp = Console.ReadLine();
+                        double T = double.Parse(Userinput_Temp);
+
+
+                        Console.WriteLine("Skriv in vindhastigheten i km/h");
+                        Console.Write("\n");
+                        string Userinput_Vind = Console.ReadLine();
+                        double V = double.Parse(Userinput_Vind);
+
+                        double WCT;
+
+                        WCT = 13.12 + (0.6215 * T) - (11.37 * Math.Pow(V, 0.16)) + (0.3965 * T * Math.Pow(V, 0.16));
+
+                        Console.Write("\n");
+                    
+                        Console.WriteLine("Beräkna windchill-faktor");
+                        Console.Write("\n");
+                 
                         Console.Write("\n");
                         Console.WriteLine($"Det känns som {WCT:F1} Km/h"); // {F1} är förkortning på en float som är en formatsträng för att visa ett flyttal med decimaler.
                         if (WCT > -25)
