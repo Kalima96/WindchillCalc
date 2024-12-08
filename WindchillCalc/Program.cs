83b4b62851ee1dea5ba5f6eb9ea2ca244d31b42d
¨hemen¨ <h24hemka@du.se>
2024-12-08 18:00:55 +0100

    Master version V3

--- WindchillCalc/Program.cs
+++ WindchillCalc/Program.cs
@@ -4,43 +4,43 @@
     {
         static void Main(string[] args)
         {
-            bool restart = true;
+            bool restart = true; //Bool variabel satt till true för att styra att programmet ska startas om sålänge användaren anger Ja
 
-            while (restart)
+            while (restart) // while loopen delar samma variabel som boolen och håller programmet igång tills användaren väljer att avsluta
             {
 
                 Console.WriteLine("Vill du starta windchillberäknaren?");
                 Console.WriteLine("Ange Ja/Nej\n");
 
-                string UserInput = Console.ReadLine().ToLower();
-                while (UserInput != "ja" && UserInput != "nej")
+                string UserInput = Console.ReadLine().ToLower(); // skapar en string som sparar användarens val (ja) eller (nej)
+                while (UserInput != "ja" && UserInput != "nej") // En till while loop som kontrollerar att användaren anger endast ja eller nej
                 {
-                UserInput = Console.ReadLine().ToLower();
-
+                UserInput = Console.ReadLine().ToLower(); // .ToLower() Gör så att stora bokstäver skrivs om till små i konsolen
+                    
                 }
-                switch (UserInput)
+                switch (UserInput) //Switch satsen Hanterar användarens val. Ifall de skriver ja så kommer programmet att börja räkna ut windchill, nej så avslutas det
                 {
-                    case "ja":
-                        Console.WriteLine("Skriv in lufttemperaturen i Celsius");
-                        Console.Write("\n");
-
-                        string Userinput_Temp = Console.ReadLine();
-                        double T = double.Parse(Userinput_Temp);
+                    case "ja": // Ett verktyg för att hantera olika värden av en variabel. Koden körs om användaren skriver ja.
+                        Console.WriteLine("Skriv in lufttemperaturen i Celsius\n");
+                        string Userinput_Temp = Console.ReadLine(); // String variabel som sparar användarens val av temperatur.
+                        double T = double.Parse(Userinput_Temp); // double T Håller lufttemperaturen som användaren matar in i Celsius som ett numeriskt värde.
 
 
-                        Console.WriteLine("Skriv in vindhastigheten i km/h");
-                        Console.Write("\n");
-                        string Userinput_Vind = Console.ReadLine();
-                        double V = double.Parse(Userinput_Vind);
+                        Console.WriteLine("Skriv in vindhastigheten i km/h\n");
+                        string Userinput_Vind = Console.ReadLine(); // String variabel som sparar användarens val av vindhastighet
+                        double V = double.Parse(Userinput_Vind); // double V Håller vindhastigheten som användaren matar in i Km/h som ett numeriskt värde.
 
-                        double WCT;
+                        double WCT; //  double variabel för att lagra den beräknade windchill-faktorn.
 
+                        // Formeln som används för att beräkna windchill temperaturen
                         WCT = 13.12 + (0.6215 * T) - (11.37 * Math.Pow(V, 0.16)) + (0.3965 * T * Math.Pow(V, 0.16));
 
                         Console.Write("\n");                  
                         Console.WriteLine("Uträkning klar\n");
                         
                         Console.WriteLine($"Det känns som {WCT:F1} Km/h"); // {F1} är förkortning på en float som är en formatsträng för att visa ett flyttal med decimaler.
+                        
+                        // Sorterar resultatet beroende på användarens val och vad beräkningen av Windchill temperaturen blir med hjälp av if och else sats.
                         if (WCT > -25)
                         {
                             Console.WriteLine("Kallt\n");
@@ -58,11 +58,11 @@
                             Console.WriteLine("Stor risk för frostskada\n");
                         }
 
-                        break;
+                        break; // används för att avsluta caset och hoppar ut ur switch satsen
 
 
-                    case "nej":
-                        restart = false;
+                    case "nej": // Om användaren väljer "nej" avslutas programmet.
+                        restart = false; // Bool variabeln blir till false om användaren väljer nej vilket avslutar loopen och programmet.
                         Console.Write("\n");
                         Console.WriteLine("Programmet avslutas. Tack och hej!");
 
